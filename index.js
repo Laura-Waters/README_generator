42481dd55df923a2773d14f81d3d15997777c2c2
Laura <laurawatersalston@gmail.com>
2024-06-23 16:11:01 -0400

    license list

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ inquirer
         name: 'usage',
     },
     {
+        type: "list",
+        message: "What license did you select for your project?",
+        name: "license",
+        choices: ["MIT License", "GNU General Public License", "GNU Lesser General Public License", "Apache License 2.0", "Mozilla Public License 2.0", "BSD License", "Creative Commons License"],
+    }, 
+    {
         type: 'input',
         message: ('Input any collaborators or other parties that should be credited for your project.'),
         name: 'credits',
@@ -45,6 +51,29 @@ inquirer
     writeToLog(htmlString);
     }
 );
+
+const formattedResponse = function(response) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/style.css"></link>
+        <title>${response.name}</title>
+    </head>
+    <body>
+        <header>
+         <h1>${response.name}</h1>
+        </header>
+        <section>
+            <h2>About Me</h2>
+            <p class="about-me-section">Hi, my name is ${response.name}.<br>I'm a ${response.profession} based in ${response.location}.<br>To view some of my recent work, checkout my GitHub: <a href="https://github.com/${response.github}" target="_blank">${response.github}</a> or my LinkedIn: <a href="https://${response.linkedin}" target="_blank">${response.linkedin}.</a></p>
+        </section>
+    </body>
+    </html>`;
+};
+
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
